48305c58b7afeaa901411fde361d0d82f20e98ad
ranadar87 <ranadar777@gmail.com>
2026-04-06 01:36:09 +0200

    Update index.js124

--- index.js
+++ index.js
@@ -225,6 +225,6 @@ app.get("/health", (req, res) => {
   });
 });
 
-app.listen(process.env.PORT || 8080, () =>
-  console.log("WA Server running on port", process.env.PORT || 8080)
+app.listen(process.env.PORT || 8002, () =>
+  console.log("WA Server running on port", process.env.PORT || 8002)
 );
